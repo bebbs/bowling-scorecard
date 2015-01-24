ee87411cd76861aaac98d1c1786bc739d10ff929
Josh Bebbington <josh@focalwebsites.com>
2015-01-24 12:38:37 +0000

    Refactored score handling methods

--- js/bowlingGame.js
+++ js/bowlingGame.js
@@ -9,15 +9,28 @@ bowlingGame.prototype.roll = function(pins) {
 bowlingGame.prototype.score = function() {
   var result = 0;
   var rollIndex = 0;
+  var game = this;
 
   for(var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (this.rolls[rollIndex] + this.rolls[rollIndex+1] === 10) {
-      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    if (isASpare()) {
+      result += spareScore();
     } else {
-      result += this.rolls[rollIndex] + this.rolls[rollIndex+1];
+      result += normalScore();
     }
     rollIndex += 2;
   };
 
   return result;
+
+  function isASpare() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex+1] === 10;
+  };
+
+  function spareScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  };
+
+  function normalScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex+1];
+  }
 };
