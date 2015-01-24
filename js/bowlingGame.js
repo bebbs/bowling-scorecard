2c06e294f41b7f07374380db0387f4e15cfdbb5a
Josh Bebbington <josh@focalwebsites.com>
2015-01-24 12:23:37 +0000

    Can roll a game of ones, refactored tests

--- js/bowlingGame.js
+++ js/bowlingGame.js
@@ -1,11 +1,16 @@
 var bowlingGame = function() {
-
+  this.rolls = [];
 };
 
 bowlingGame.prototype.roll = function(pins) {
-
+  this.rolls.push(pins);
 };
 
 bowlingGame.prototype.score = function() {
-  return 0;
-}
+  var result = 0;
+  for(var i = 0; i < 20; i++) {
+    result += this.rolls[i];
+  };
+
+  return result;
+};
--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -2,13 +2,24 @@ describe("A bowling game", function() {
 
   var game;
 
-  it("can score a gutter game", function() {
-    var game = new bowlingGame();
-    for(var i = 0; i < 20; i++) {
-      game.roll(0);
-    };
+  beforeEach(function() {
+    game = new bowlingGame();
+  });
 
+  it("can score a gutter game", function() {
+    rollMultiple(0,20);
     expect(game.score()).toEqual(0);
   });
+
+  it("can roll a game of ones", function() {
+    rollMultiple(1,20);
+    expect(game.score()).toEqual(20);
+  });
+
+  var rollMultiple = function(pins, rolls) {
+    for(var i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  };
 
 });
