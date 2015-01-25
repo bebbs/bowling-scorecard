da98415f0163b27fda1702fa61d9e9892cad8fc3
Josh Bebbington <josh@focalwebsites.com>
2015-01-25 11:56:50 +0000

    Game can keep a running total

--- js/bowlingGame.js
+++ js/bowlingGame.js
@@ -1,5 +1,6 @@
 var bowlingGame = function() {
   this.rolls = [];
+  this.rollIndex = 0;
 };
 
 bowlingGame.prototype.roll = function(pins) {
@@ -8,41 +9,53 @@ bowlingGame.prototype.roll = function(pins) {
 
 bowlingGame.prototype.score = function() {
   var result = 0;
-  var rollIndex = 0;
   var game = this;
 
   for(var frameIndex = 0; frameIndex < 10; frameIndex++) {
     if (isAStrike()) {
       result += strikeScore();
-      rollIndex++;
+      game.rollIndex++;
     } else if (isASpare()) {
       result += spareScore();
-      rollIndex += 2;
+      game.rollIndex += 2;
     } else {
       result += normalScore();
-      rollIndex += 2;
+      game.rollIndex += 2;
     }
   };
 
   return result;
 
   function isAStrike() {
-    return game.rolls[rollIndex] === 10;
+    return game.rolls[game.rollIndex] === 10;
   };
 
   function isASpare() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex+1] === 10;
+    return game.rolls[game.rollIndex] + game.rolls[game.rollIndex+1] === 10;
   };
 
   function strikeScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex+1] + game.rolls[rollIndex+2];
+    return game.rolls[game.rollIndex] + game.rolls[game.rollIndex+1] + game.rolls[game.rollIndex+2];
   };
 
   function spareScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex+1] + game.rolls[rollIndex + 2];
+    return game.rolls[game.rollIndex] + game.rolls[game.rollIndex+1] + game.rolls[game.rollIndex + 2];
   };
 
   function normalScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex+1];
+    return game.rolls[game.rollIndex] + game.rolls[game.rollIndex+1];
   }
+};
+
+bowlingGame.prototype.runningTotal = function() {
+  var game = this;
+  var rollsLength = this.rolls.length;
+
+  function addEmptyRolls() {
+    for(var i = rollsLength; i < 20; i++) {
+      game.roll(0);
+    };
+  };
+  addEmptyRolls();
+  return this.score();
 };
--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -6,30 +6,63 @@ describe("A bowling game", function() {
     game = new bowlingGame();
   });
 
-  it("can score a gutter game", function() {
-    rollMultiple(0,20);
-    expect(game.score()).toEqual(0);
+  describe("Scoring a game", function() {
+
+    it("can score a gutter game", function() {
+      rollMultiple(0,20);
+      expect(game.score()).toEqual(0);
+    });
+
+    it("can roll a game of ones", function() {
+      rollMultiple(1,20);
+      expect(game.score()).toEqual(20);
+    });
+
+    it("can roll a spare", function() {
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      rollMultiple(0, 17);
+      expect(game.score()).toEqual(16);
+    })
+
+    it("can roll a strike", function() {
+      game.roll(10);
+      game.roll(4);
+      game.roll(3);
+      rollMultiple(0, 16);
+      expect(game.score()).toEqual(24);
+    });
+
+    it("can score a perfect game", function() {
+      rollMultiple(10,12);
+      expect(game.score()).toEqual(300);
+    });
+
   });
 
-  it("can roll a game of ones", function() {
-    rollMultiple(1,20);
-    expect(game.score()).toEqual(20);
-  });
+  describe("Keeping a running total", function() {
 
-  it("can roll a spare", function() {
-    game.roll(5);
-    game.roll(5);
-    game.roll(3);
-    rollMultiple(0, 17);
-    expect(game.score()).toEqual(16);
-  })
+    it("after 1 frame", function() {
+      game.roll(1);
+      game.roll(1);
+      expect(game.runningTotal()).toEqual(2);
+    });
 
-  it("can roll a strike", function() {
-    game.roll(10);
-    game.roll(4);
-    game.roll(3);
-    rollMultiple(0, 16);
-    expect(game.score()).toEqual(24);
+    it("after a spare", function() {
+      game.roll(3);
+      game.roll(7);
+      game.roll(5);
+      expect(game.runningTotal()).toEqual(20);
+    });
+
+    it("after a strike", function() {
+      game.roll(10);
+      game.roll(4);
+      game.roll(4);
+      expect(game.runningTotal()).toEqual(26);
+    });
+
   });
 
   var rollMultiple = function(pins, rolls) {
