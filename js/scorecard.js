ba6ea7a77bec010107b29d14d12fdaa14e8473fc
Josh Bebbington <josh@focalwebsites.com>
2015-02-02 12:13:28 +0000

    Refactoring

--- js/scorecard.js
+++ js/scorecard.js
@@ -1,10 +1,28 @@
 var Scorecard = function() {
   this.frames = [];
+  this.totalScore = 0;
 };
 
 Scorecard.prototype.setupGame = function() {
   this._populateFrames();
   this._generateFrameIndex();
+};
+
+Scorecard.prototype.calculateScore = function() {
+  for(var i=0; i<10; i++) {
+    if (i !== 0) {
+      this.calculateBonusScore(i);
+    };
+    this.totalScore += this.frames[i].totalScore;
+  };
+};
+
+Scorecard.prototype.calculateBonusScore = function(x) {
+  if (this.frames[x-1]._isAStrike()) {
+    this.frames[x-1].totalScore += this.frames[x].totalScore;
+  } else if (this.frames[x-1]._isASpare()) {
+    this.frames[x-1].totalScore += this.frames[x].firstRollScore;
+  };
 };
 
 Scorecard.prototype._populateFrames = function() {
--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -7,12 +7,48 @@ describe("Scorecard", function() {
     scorecard.setupGame();
   });
 
-  it("should create a game of 10 frames", function() {
-    expect(scorecard.frames.length).toEqual(10);
+  describe("by default", function() {
+
+    it("should create a game of 10 frames", function() {
+      expect(scorecard.frames.length).toEqual(10);
+    });
+
+    it("each frame should have an index", function() {
+      expect(scorecard.frames[0].number).toEqual(1);
+    });
+
+    it("should have a total score of 0", function() {
+      expect(scorecard.totalScore).toEqual(0);
+    });
   });
 
-  it("each frame should have an index", function() {
-    expect(scorecard.frames[0].number).toEqual(1);
+  describe("scoring the game", function() {
+
+    it("can calculate the score after 1 frame", function() {
+      scorecard.frames[0].receiveRollOne(1);
+      scorecard.frames[0].receiveRollTwo(1);
+      scorecard.calculateScore();
+      expect(scorecard.totalScore).toEqual(2);
+    });
+
+    it("can calculate the score after a strike", function() {
+      scorecard.frames[0].receiveRollOne(5);
+      scorecard.frames[0].receiveRollTwo(2);
+      scorecard.frames[1].receiveRollOne(10);
+      scorecard.frames[2].receiveRollOne(4);
+      scorecard.frames[2].receiveRollTwo(5);
+      scorecard.calculateScore();
+      expect(scorecard.totalScore).toEqual(35);
+    });
+
+    it("can calculate the score after a spare", function() {
+      scorecard.frames[0].receiveRollOne(1);
+      scorecard.frames[0].receiveRollTwo(9);
+      scorecard.frames[1].receiveRollOne(1);
+      scorecard.calculateScore();
+      expect(scorecard.totalScore).toEqual(12);
+    });
+
   });
 
 });
